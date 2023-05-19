f19b4a4f478287a45fdf52a2c3e71b3b2200829a
spicious225 <ryanthelion.c@gmail.com>
2023-05-19 00:40:37 +0000

    updated tables

---
+++ operations_app/operations_app/doctype/employee_workstation_expertise/employee_workstation_expertise.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Ryan Chau and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Employee Workstation Expertise', {
+	// refresh: function(frm) {
+
+	// }
+});
